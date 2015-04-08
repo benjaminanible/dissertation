ad0f25c5d8d70f06d763c08c7b840ff65b4307b5
Gemma Anible <gemma@ramblinations.com>
2015-04-08 00:31:08 -0600

    make sure the experiment dies on errors

--- asl_english_trials.py
+++ asl_english_trials.py
@@ -4,26 +4,27 @@ from asl_english_trials import PictureNaming
 from asl_english_trials.VideoInput import VideoInput
 
 if __name__ == '__main__':
-    exp = e.design.Experiment(name="Protocol 3: Picture Naming")
+    try:
+        exp = e.design.Experiment(name="Protocol 3: Picture Naming")
 
-    for block in PictureNaming.blocks:
-        exp.add_block(block)
+        for block in PictureNaming.blocks:
+            exp.add_block(block)
 
-    e.control.initialize(exp)
-    exp.data_variable_names = ['picture name', 'video filename', 'type', 'reaction time']
+        e.control.initialize(exp)
+        exp.data_variable_names = ['picture name', 'video filename', 'type', 'reaction time']
 
-    for block in exp.blocks:
-        for trial in block.trials:
-            trial.preload_stimuli()
+        for block in exp.blocks:
+            for trial in block.trials:
+                trial.preload_stimuli()
 
-    device = cv2.VideoCapture(0)
-    device.release()
+        device = cv2.VideoCapture(0)
+        device.release()
 
-    e.control.start(exp)
-    e.control.stop_audiosystem()
+        e.control.start(exp)
+        e.control.stop_audiosystem()
 
-    for block in exp.blocks:
-        for trial in block.trials:
-             trial.present_callback(trial, exp, device)
-
-    e.control.end(system_exit=True)
+        for block in exp.blocks:
+            for trial in block.trials:
+                 trial.present_callback(trial, exp, device)
+    finally:
+        e.control.end(system_exit=True)
